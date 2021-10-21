bacc31f2cd040ce6590877047cb2482b70cc2e7c
alex-chenyi-zhang <chzhang@sissa.it>
2021-10-21 15:20:11 +0200

    Wrote function that computes neighbour list and wrote outer constructors for the polymer and MC_data structures to lighten the notation when defining new objects of these types

--- magpoly/src/structures_and_functions.jl
+++ magpoly/src/structures_and_functions.jl
@@ -1,5 +1,5 @@
 using Random, Distributions
-Random.seed!(2021)
+Random.seed!()
 
 # Here I define the global quantities used by all pivot MCMC run which are the pivot moves
 const pivot_moves = Array{Int,3}(zeros(47, 2, 3)) # Tensor that defines all the pivot moves of the octahedral symmetry group
@@ -38,6 +38,13 @@ struct Magnetic_polymer{T1<:Int, T2<:AbstractFloat}
     coord::Array{T1,2}         # Coordinates of the monomers
     trial_coord::Array{T1,2}
     hash_saw::Dict{Int64, Int64}
+    neighbours::Array{T1, 2}
+    trial_neighbours::Array{T1, 2}
+end
+
+function Magnetic_polymer(n_mono::T1, inv_temp::T2, J::T2, alpha::T2) where {T1<:Int, T2<:AbstractFloat}
+    Magnetic_polymer(n_mono, inv_temp, J, alpha, zeros(Int, n_mono), zeros(n_mono), zeros(Int, 3,n_mono),
+    zeros(Int, 3,n_mono), Dict{Int, Int}(), zeros(Int, 7,n_mono), zeros(Int, 7,n_mono))
 end
 
 function initialize_poly!(poly::Magnetic_polymer)
@@ -80,7 +87,12 @@ struct MC_data{T1<:Int, T2<:AbstractFloat}
     n_steps::T1
     energies::Array{T2}
     magnetization::Array{T2}
+    rg2::Array{T2}
     re2::Array{T1}
+end
+
+function MC_data(n_steps::Int)
+    MC_data(n_steps, zeros(n_steps), zeros(n_steps), zeros(n_steps), zeros(Int, n_steps))
 end
 
 #########################################################################
@@ -95,16 +107,16 @@ function try_pivot!(k::Int, move::Int, pol::Magnetic_polymer)
     end
     for i_mono in k+1:pol.n_mono
         for j in 1:3
-            #println(pivot_moves[move,1,j])
             pol.trial_coord[j,i_mono] = (pivot_moves[move,2,j]*(pol.coord[pivot_moves[move,1,j],i_mono]
                                          - pol.coord[pivot_moves[move,1,j],k]) + pol.coord[j,k])
         end
     end
 end
 
+#########################################################################
 
 function checksaw!(k::Int, pol::Magnetic_polymer, a::Int)
-    L = max(k, pol.n_mono-k)
+    L = max(k, pol.n_mono-k) + 1
     is_saw = true
     t = 0
     while(t<L && is_saw)
@@ -129,6 +141,54 @@ function checksaw!(k::Int, pol::Magnetic_polymer, a::Int)
     return is_saw
 end
 
+#########################################################################
+## poi quando si faranno le mosse locali non si userà questa funnzione perché 
+## non ci sarà bisogno di ricalcolare TUTTA la lista dei vicini
+function compute_neighbours!(coo::Array{Int,2}, near::Array{Int,2}, dic::Dict{Int,Int}, a::Int, n_mono::Int)
+    for i_mono in 1:n_mono
+        near[1, i_mono] = 0
+        near[2:7, i_mono] .= -1
+
+        neigh_coord = coo[:, i_mono] # remember that slices in julia create a copy
+
+        for j in 1:3
+            for k in -1:2:1
+                neigh_coord[j] += k
+                saw_key = neigh_coord[1]*a^2 + neigh_coord[2]*a + neigh_coord[3]
+                if haskey(dic, saw_key)
+                    near[1, i_mono] += 1
+                    n_neigh = near[1, i_mono]+1
+                    near[n_neigh, i_mono] = dic[saw_key]
+                end
+                neigh_coord[j] -= k
+            end
+        end
+    end
+end
+
+#########################################################################
+
+function gyration_radius_squared(pol::Magnetic_polymer)
+    #rcm = sum(pol.coord, dims=2)./pol.n_mono
+    rcm = zeros(3)
+    rg2 = 0.0
+    for i in 1:pol.n_mono
+        for j in 1:3
+            #rg2 += sum((view(pol.coord,:,i) .- rcm).^2)
+            rg2 += pol.coord[j,i]^2
+            rcm[j] += pol.coord[j,i]
+        end
+    end
+    r = 0.0
+    for j in 1:3
+        r += rcm[j]^2
+    end
+    return rg2/pol.n_mono - r/(pol.n_mono)^2
+end
+
+
+#########################################################################
+#########################################################################
 
 function MC_run!(pol::Magnetic_polymer, traj::MC_data)
     n_acc = 0
@@ -142,8 +202,16 @@ function MC_run!(pol::Magnetic_polymer, traj::MC_data)
       cheaper than hashing keys which are arrays sliced from a matrix 
     =#
     hash_base = 2*pol.n_mono + 1
+
+    #checksaw!(1, pol, hash_base)
+    #compute_neighbours!(pol.coord, pol.neighbours, pol.hash_saw, hash_base, pol.n_mono)
+    #println(pol.hash_saw)
+    #for i in 1:pol.n_mono
+    #    println(view(pol.neighbours,:,i))
+    #end
+
     for i_step in 1:traj.n_steps
-        pivot = rand(2:pol.n_mono)
+        pivot = rand(2:pol.n_mono-1)
         p_move = rand(1:47)
         if i_step%100000 == 0
             println("Pivot: ", pivot)
@@ -160,12 +228,14 @@ function MC_run!(pol::Magnetic_polymer, traj::MC_data)
                 end
             end
         end
-        traj.re2[i_step] = sum((pol.coord[:,1].-pol.coord[:,end]).^2)
+        for j in 1:3
+            traj.re2[i_step] += pol.coord[j,end]^2 
+        end
+        traj.rg2[i_step] = gyration_radius_squared(pol)
         empty!(pol.hash_saw)
         
     end
-    println("Fraction of accepted moves: ", n_acc)
-    #println(mean(sqrt.(traj.re2[500000:end])))
+    println("Fraction of accepted moves: ", n_acc/traj.n_steps)
 end
 
 
